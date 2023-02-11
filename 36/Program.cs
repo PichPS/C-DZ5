2039fc40ef91300a382b7a0b72323255f48fe366
Pavel_Pichugin <pichps@mail.ru>
2023-02-11 14:01:33 +0300

    36 готова

---
+++ 36/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+
+// [3, 7, 23, 12] -> 19
+
+// [-4, -6, 89, 6] -> 0
+
+int[] EnterArr()
+{
+    System.Console.WriteLine("Введите количество элементов массива");
+    int n = Convert.ToInt32(Console.ReadLine());
+    System.Console.WriteLine("Введите меньшее число массива");
+    int a = Convert.ToInt32(Console.ReadLine());
+    System.Console.WriteLine("Введите большее число массива");
+    int b = Convert.ToInt32(Console.ReadLine());
+    int[] Rand = new int[n];
+    for (int i = 0; i < Rand.Length; i++)
+    {
+        Rand[i] = new Random().Next(a, (b + 1));
+    }
+    System.Console.Write($"[{string.Join(", ", Rand)}] -> ");
+    return Rand;
+}
+
+int OddNumbersSum (int[] Arr)
+{
+    int Sum = 0;
+    for (int i = 1; i < Arr.Length; i+=2)
+    {
+    Sum = Sum + Arr[i];
+    }
+System.Console.WriteLine(Sum);
+return Sum;
+}
+
+OddNumbersSum(EnterArr());
